9fc9ec27b62e1bf8eef90ed3be875a757ff22e58
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-07-25 13:22:20 +0500

    commit

---
+++ src/main/java/org/example/MainClient.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainClient {
+
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8080;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            out.println("netology.ru");
+            String resp = in.readLine();
+            System.out.println(resp);;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
